10982cd8ee11f0b13f72fe1652173bc35e3a1176
Matt <111116215+matt-minev@users.noreply.github.com>
2025-07-21 10:01:28 +0300

    Create cleanup.py
    A handy utility to quickly delete _pycache_ folders.

---
+++ cleanup.py
@@ -0,0 +1,9 @@
+import os
+import shutil
+
+for root, dirs, files in os.walk('.', topdown=False):
+    for name in dirs:
+        if name == '__pycache__':
+            dir_path = os.path.join(root, name)
+            print(f"Deleting {dir_path}")
+            shutil.rmtree(dir_path)
